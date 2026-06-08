4c1c80e8eb094cc08cb7a36c525be6de0c89614f
William Ortega <wogutierrez@gmail.com>
2026-06-08 16:31:43 -0400

    new

--- app.py
+++ app.py
@@ -1,8 +1,16 @@
 # Import libraries
+from frask import Flask, redirect, request, render_template, url_for
 
 # Instantiate Flask functionality
+app = Flask(__name__)
 
 # Sample data
+transactions = [
+    {'id': 1, 'date': '2023-06-01', 'amount': 100},
+    {'id': 2, 'date': '2023-06-02', 'amount': -200},
+    {'id': 3, 'date': '2023-06-03', 'amount': 300}
+]
+
 
 # Read operation
 
