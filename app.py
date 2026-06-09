2e20837d3e2c51873320fae6732ac8b3edd5173d
William Ortega <wogutierrez@gmail.com>
2026-06-09 08:31:19 -0400

    update done

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 # Import libraries
-from frask import Flask, redirect, request, render_template, url_for
+from flask import Flask, redirect, request, render_template, url_for
 
 # Instantiate Flask functionality
 app = Flask(__name__)
@@ -24,20 +24,38 @@ def add_transaction():
     
     if request.method == 'POST':
         transaction = {
-            'id': len(transaction) + 1,
-            'date':request.form['date'],
+            'id': len(transactions) + 1,
+            'date': request.form['date'],
             'amount': float(request.form['amount']) 
         }
 
         transactions.append(transaction)
 
         return redirect(url_for("get_transactions"))
-        
-return render_template("form.html")
 
-
+    return render_template("form.html")
 
 # Update operation
+@app.route("/edit/<int:transaction_id>", methods=["GET","POST"])
+def edit_transaction(transaction_id):
+
+    if request.method == 'POST':
+        date = request.form['date']
+        amount = float(request.form['amount'])
+
+        for transaction in transactions:
+            if transaction['id'] == transaction_id:
+                transaction['date'] = date
+                transaction['amount'] = amount
+                break
+        
+        return redirect(url_for("get_transactions"))
+
+    for transaction in transactions:
+        if transaction_id == transaction['id']:
+            return render_template("edit.html", transaction=transaction)
+    
+    return {"message": "Transaction not found"}, 404
 
 # Delete operation
 
